8405c00b94340ae32ed96ee656b95e77cd0aae3e
Rafael Azevedo <rrazvd@gmail.com>
2023-04-08 16:34:57 -0300

    Refactor text segmenter

--- src/job/streams/text-segmenter.js
+++ src/job/streams/text-segmenter.js
@@ -1,23 +1,20 @@
 import { Transform } from 'stream'
 import _get from 'lodash/get'
 
-const sanitizeArray = (arr) => arr.filter((word) => word !== '')
+const getStringArray = (str) => str
+  .toLowerCase()
+  .split(' ')
+  .filter((word) => word !== '')
 
 const getNewWords = (oldString, newString) => {
-  const newStringArray = sanitizeArray(newString.split(' '))
-  const oldStringArray = sanitizeArray(oldString.split(' '))
+  const newStringArray = getStringArray(newString)
+  const oldStringArray = getStringArray(oldString)
 
   const newStringArrayLength = newStringArray.length
   const oldStringArrayLength = oldStringArray.length
 
   const wordsDiff = newStringArrayLength - oldStringArrayLength
   const startsWithSameWord = newStringArray[0] === oldStringArray[0]
-
-  /*   console.log('------------------------------')
-  console.log(`Old string: ${oldStringArray.join(' ')}\n`)
-  console.log(`New string: ${newStringArray.join(' ')}\n`)
-  console.log(`Length diff: ${wordsDiff}`)
-  console.log(`Start with same word: ${startsWithSameWord}`) */
 
   if (wordsDiff > 0) {
     const words = newStringArray.slice(oldStringArrayLength)
@@ -28,15 +25,16 @@ const getNewWords = (oldString, newString) => {
     return startsWithSameWord ? [] : newStringArray
   }
 
+  if (oldStringArray.join(' ') === newStringArray.join(' ')) return []
+
   const newStringLastWord = newStringArray[newStringArrayLength - 1]
   const oldStringLastWord = oldStringArray[oldStringArrayLength - 1]
 
-  const isSubstring = newString.includes(oldString)
-  // console.log(`Contains same sentence: ${isSubstring}`)
+  const isLastWordIncomplete = newStringLastWord.startsWith(oldStringLastWord)
 
-  if (isSubstring) return newStringLastWord === oldStringLastWord ? [] : [newStringLastWord]
+  if (startsWithSameWord && isLastWordIncomplete) return [newStringLastWord]
 
-  return startsWithSameWord ? [] : newStringArray
+  return newStringArray
 }
 
 class TextSegmenter extends Transform {
@@ -49,14 +47,10 @@ class TextSegmenter extends Transform {
   _transform(chunk, _, next) {
     const currentTranscript = _get(chunk, 'results[0].alternatives[0].transcript')
 
-    const newWords = getNewWords(
-      this.previousTranscript.toLowerCase(),
-      currentTranscript.toLowerCase()
-    )
-
-    // console.log('New words: ', newWords.join(' '), '\n')
+    const newWords = getNewWords(this.previousTranscript, currentTranscript)
 
     newWords.forEach((word) => this.push(word))
+
     this.previousTranscript = currentTranscript
     next()
   }
