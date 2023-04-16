15251aa5a1f2b17ac454acff89107f46134c635d
Rafael Azevedo <rrazvd@gmail.com>
2023-04-15 23:10:51 -0300

    Refactor song matcher

--- src/job/streams/song-matcher.js
+++ src/job/streams/song-matcher.js
@@ -1,18 +1,13 @@
 import { Transform } from 'stream'
 
 class SongMatcher extends Transform {
-  static database = undefined
-
-  WINDOW_SIZE
-
-  window = []
-
-  matchedIds = {}
-
   constructor(database, windowSize) {
     super({ objectMode: true })
+
     this.database = database
     this.WINDOW_SIZE = windowSize
+    this.window = []
+    this.matchedIds = {}
   }
 
   _transform(chunk, _, next) {
@@ -20,48 +15,50 @@ class SongMatcher extends Transform {
 
     // query the database with the current transcribed word
     this.database.all(`SELECT mxm_tid FROM lyrics where word='${word}'`, (err, rows) => {
-      // increase a counter for each matched id
-      rows.forEach(({ mxm_tid: musicId }) => {
-        if (this.matchedIds[musicId] === undefined) {
-          this.matchedIds[musicId] = 1
-          return
-        }
-        this.matchedIds[musicId] += 1
-      })
-
       // update window with new matched ids
       this.window.push(rows)
+
+      // increase a counter for each matched id
+      rows.forEach(({ mxm_tid: songId }) => {
+        if (this.matchedIds[songId] !== undefined) {
+          this.matchedIds[songId] += 1
+          return
+        }
+        this.matchedIds[songId] = 1
+      })
 
       if (this.window.length > this.WINDOW_SIZE) {
         // update window removing expired matched ids
         const expiredMatchedIds = this.window.shift()
 
         // decrease a counter for each expired matched id
-        expiredMatchedIds.forEach(({ mxm_tid: musicId }) => {
-          if (this.matchedIds[musicId] > 1) {
-            this.matchedIds[musicId] -= 1
+        expiredMatchedIds.forEach(({ mxm_tid: songId }) => {
+          if (this.matchedIds[songId] > 1) {
+            this.matchedIds[songId] -= 1
             return
           }
-          delete this.matchedIds[musicId]
+          delete this.matchedIds[songId]
         })
       }
     })
 
     // compute a descending ranking
-    const ranking = Object
-      .keys(this.matchedIds)
-      .sort((a, b) => this.matchedIds[b] - this.matchedIds[a])
-      .slice(0, 5)
-      .map((musicId, index) => (
-        {
-          musicId,
-          score: Math.round((this.matchedIds[musicId] / this.WINDOW_SIZE) * 100),
-          position: index + 1
-        }
-      ))
+    const ranking = this.getRanking()
 
     this.push(`${JSON.stringify(ranking, null, 2)}\n\n`)
     next()
+  }
+
+  calculateScore(songId) {
+    return Math.round((this.matchedIds[songId] / this.WINDOW_SIZE) * 100)
+  }
+
+  getRanking() {
+    return Object
+      .keys(this.matchedIds)
+      .sort((a, b) => this.matchedIds[b] - this.matchedIds[a])
+      .slice(0, 5)
+      .map((songId) => ({ songId, score: this.calculateScore(songId) }))
   }
 }
 
