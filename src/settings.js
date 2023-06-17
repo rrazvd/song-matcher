0916fb6817dfdb63ac182629fe936d5fe3339362
Rafael Azevedo <rrazvd@gmail.com>
2023-06-17 19:08:54 -0300

    Refactor settings lint

--- src/settings.js
+++ src/settings.js
@@ -1,19 +1,19 @@
 const {
   SERVER_PORT = 3000,
   STREAMING_URL = '',
-  STREAM_SAMPLE_RATE_HERTZ = 48000,
   LANGUAGE_CODE = 'pt-BR',
   SONG_MATCHER_WINDOW_SIZE = 30,
+  STREAM_SAMPLE_RATE_HERTZ = 48000,
   AUDIO_TRANSCRIPT_RESTART_INTERVAL = 300 * 1000
 } = process.env
 
 const STREAMING_RECOGNITION_CONFIG = {
   config: {
     encoding: 'FLAC',
-    sampleRateHertz: STREAM_SAMPLE_RATE_HERTZ,
-    languageCode: LANGUAGE_CODE,
     model: 'latest_long',
-    audioChannelCount: 1
+    audioChannelCount: 1,
+    languageCode: LANGUAGE_CODE,
+    sampleRateHertz: STREAM_SAMPLE_RATE_HERTZ
   },
   interimResults: true
 }
@@ -23,8 +23,8 @@ const MXM_DATASET_PATH = './src/databases/mxm_dataset.db'
 export {
   SERVER_PORT,
   STREAMING_URL,
-  STREAMING_RECOGNITION_CONFIG,
   MXM_DATASET_PATH,
   SONG_MATCHER_WINDOW_SIZE,
+  STREAMING_RECOGNITION_CONFIG,
   AUDIO_TRANSCRIPT_RESTART_INTERVAL
 }
